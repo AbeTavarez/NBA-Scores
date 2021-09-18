2b77c81bf53901e9d0574f0dcb763a3d459ee0e9
abeTavarez <abeincwireless@gmail.com>
2021-09-18 10:52:08 -0400

    re-factor code

--- app.js
+++ app.js
@@ -1,113 +1,127 @@
 // import { gamesData } from './data/sample-data';
 
-const warriorsGames = [{
+const warriorsGames = [
+  {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 119,
-      isWinner: true
+      isWinner: true,
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 106,
-      isWinner: false
-    }
+      isWinner: false,
+    },
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 105,
-      isWinner: false
+      isWinner: false,
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 127,
-      isWinner: true
-    }
+      isWinner: true,
+    },
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 126,
-      isWinner: true
+      isWinner: true,
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 85,
-      isWinner: false
-    }
+      isWinner: false,
+    },
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 92,
-      isWinner: false
+      isWinner: false,
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 95,
-      isWinner: true
-    }
+      isWinner: true,
+    },
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 94,
-      isWinner: false
+      isWinner: false,
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 98,
-      isWinner: true
-    }
+      isWinner: true,
+    },
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 115,
-      isWinner: true
+      isWinner: true,
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 86,
-      isWinner: false
-    }
+      isWinner: false,
+    },
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 101,
-      isWinner: true
+      isWinner: true,
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 92,
-      isWinner: false
-    }
-  }
+      isWinner: false,
+    },
+  },
 ];
- // * ============================== Code
+// * ============================== Code
 
-const ul = document.createElement('ul');
+// =========================== Make Chart
 
-for (let game of warriorsGames) {
-    const li = document.createElement('li');
-    // destructure each game object // getting two object back
-    const {homeTeam, awayTeam} = game;
+const makeChart = (games) => {
+  const ul = document.createElement("ul");
+
+  for (let game of games) {
+    const li = document.createElement("li");
+    
+    li.innerHTML = getScoreLine(game);
+
+    // const warriors = hTeam === "Golden State" ? homeTeam : awayTeam;
+    // li.classList.add(warriors.isWinner ? "win" : "loss");
+
+    ul.append(li);
+  }
+  return ul
+};
+
+// ========================== getScoreLine
+
+const getScoreLine = ({ homeTeam, awayTeam }) => {
     // destructure team and point from each team
-    const {team: hTeam, points: hPoints} = homeTeam;
-    const {team: aTeam, points: aPoints} = awayTeam;
+    const { team: hTeam, points: hPoints } = homeTeam;
+    const { team: aTeam, points: aPoints } = awayTeam;
 
     const teamNames = `${aTeam} @ ${hTeam}`;
-    let scoreLine
+    let scoreLine;
     if (aPoints > hPoints) {
-        scoreLine = `<b>${aPoints}</b> - ${hPoints}`;
+      scoreLine = `<b>${aPoints}</b> - ${hPoints}`;
     } else {
-        scoreLine = `${aPoints} - <b>${hPoints}</b>`;
+      scoreLine = `${aPoints} - <b>${hPoints}</b>`;
     }
-    
-    li.innerHTML = `${teamNames} ${scoreLine}`
-    ul.append(li);
-    
-}
+    return `${teamNames} ${scoreLine}`;
+};
 
-document.body.prepend(ul)
+const chart1 = makeChart(warriorsGames);
+document.body.append(chart1);
